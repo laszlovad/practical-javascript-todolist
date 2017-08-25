279deff6b91b5b85f427285a699fa1d445821e1e
laszlovad <laszlo.a.vad@gmail.com>
2017-08-25 10:07:44 +0200

    add a button for changing todos

--- todolist.js
+++ todolist.js
@@ -74,6 +74,13 @@ var handlers = {
     var addTodoTextInput = document.getElementById('addTodoTextInput');
     todoList.addTodo(addTodoTextInput.value);
     addTodoTextInput.value = '';
+  },
+  changeTodo: function() {
+    var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+    var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoPositionInput = '';
+    changeTodoTextInput = '';
   }
 }
 
