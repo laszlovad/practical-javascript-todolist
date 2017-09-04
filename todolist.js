9a4d6ad968124ed36dbb495d2c8673a973681d68
laszlovad <laszlo.a.vad@gmail.com>
2017-09-04 10:07:29 +0200

    add a Delete button for each todo

--- todolist.js
+++ todolist.js
@@ -58,10 +58,8 @@ var handlers = {
     changeTodoTextInput.value = '';
     view.displayTodos();
   },
-  deleteTodo: function() {
-    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-    deleteTodoPositionInput.value = '';
+  deleteTodo: function(position) {
+    todoList.deleteTodo(position);
     view.displayTodos();
   },
   toggleCompleted: function() {
@@ -92,9 +90,36 @@ var view = {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
 
+      todoLi.id = i; // Sets the newly created todo's id to its position in the array.
       todoLi.textContent = todoTextWithCompletion; // Add the todo text and completion data to the newly created li.
+      todoLi.appendChild(this.createDeleteButton()); // Adds a delete button to the li.
       todosUl.appendChild(todoLi); // Insert the newly created li into the ul.
     }
+  },
+  createDeleteButton: function() { // It should create a button, then return it.
+    var deleteButton = document.createElement('button'); // Creates the button.
+    deleteButton.textContent = 'Delete'; // Sets its text.
+    deleteButton.className = 'deleteButton'; // Adds a class to it.
+    return deleteButton; // Returns it.
+  },
+  setUpEventListeners: function() {
+    var todosUl = document.querySelector('ul');
+
+    todosUl.addEventListener('click', function(event){
+      // Get the element that was clicked on.
+      var elementClicked = event.target;
+
+      // Check if a delete button was clicked.
+      if (elementClicked.className === 'deleteButton') {
+        handlers.deleteTodo(parseInt(elementClicked.parentNode.id)); // Parses the string into a number then runs handlers.deleteTodo.
+      }  
+    });
   }
 }
+
+view.setUpEventListeners();
+
+
+
+
 
