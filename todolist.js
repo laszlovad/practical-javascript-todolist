2864c17129cc4acbf4cc5c14aa6f626d3b8d74f6
laszlovad <laszlo.a.vad@gmail.com>
2017-09-06 09:17:37 +0200

    change for loops to forEach functions in toggleAll

--- todolist.js
+++ todolist.js
@@ -21,22 +21,24 @@ var todoList = {
     var completedTodos = 0;
     
     //Get number of completed todos.
-    for (var i = 0; i < totalTodos; i++) {
-      if (this.todos[i].completed === true) {
+    this.todos.forEach(function(todo) {
+      if (todo.completed === true) {
         completedTodos++;
       }
-    }
+    });
     
     // Case 1: If everything's true, make everything false.
     if (completedTodos === totalTodos) {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = false;
-      }
+      this.todos.forEach(function(todo) {
+        todo.completed = false;
+      });
+   
     // Case 2: Otherwise, make everything true.
     } else {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = true;
-      }
+      this.todos.forEach(function(todo) {
+        todo.completed = true;
+      });
+
     }
   }
 };
